1abe6831b1334e65e44b40d4a8a8382d9254ea62
David Varnum <varnumd@gmail.com>
2020-04-02 20:22:42 -0700

    terraform fmt

--- gcp_variables.tf
+++ gcp_variables.tf
@@ -5,41 +5,41 @@
 
 variable "gcp_credentials_file" {
   description = "Locate the GCP credentials file."
-  type = string
+  type        = string
 }
 
 variable gcp_project {
   description = "GCP Project"
-  default = "terraform-testing-272920"
+  default     = "terraform-testing-272920"
 }
 
 variable gcp_region {
   description = "Default to US Central."
-  default = "us-central1"
+  default     = "us-central1"
 }
 
 variable gcp_zone {
   description = "Default to US Central1c."
-  default = "us-central1-c"
+  default     = "us-central1-c"
 }
 
 variable on_prem_ip1 {
   description = "The IP of the on-prem VPN gateway"
-  type = string
+  default     = "UPDATE-ME"
 }
 
 variable gcp_asn {
   description = "BGP ASN or GCP Cloud Router"
-  default = 64997
+  default     = 64997
 }
 
 variable on_prem_asn {
   description = "BGP ASN or On-Prem Router"
-  default = 65000
+  default     = 65000
 }
 
 variable gcp_shared_secret {
   description = "VPN shared secret"
-  default = "d0v3r1a1d"
+  default     = "d0v3r1a1d"
 }
 
--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "google" {
-  project = var.gcp_project
-  region = var.gcp_region
-  zone = var.gcp_zone
+  project     = var.gcp_project
+  region      = var.gcp_region
+  zone        = var.gcp_zone
   credentials = var.gcp_credentials_file
 }
 
 provider "google-beta" {
-  project = var.gcp_project
-  region = var.gcp_region
-  zone = var.gcp_zone
+  project     = var.gcp_project
+  region      = var.gcp_region
+  zone        = var.gcp_zone
   credentials = var.gcp_credentials_file
 }
 
@@ -17,14 +17,14 @@ provider "google-beta" {
  */
 
 resource "google_compute_ha_vpn_gateway" "ha_gateway1" {
-  provider              = google-beta
+  provider = google-beta
   region   = "us-central1"
   name     = "ha-vpn-1"
   network  = google_compute_network.tf_vpc_net1.self_link
 }
 
 resource "google_compute_external_vpn_gateway" "external_gateway" {
-  provider              = google-beta
+  provider        = google-beta
   name            = "hq-cisco"
   redundancy_type = "SINGLE_IP_INTERNALLY_REDUNDANT"
   description     = "An externally managed VPN gateway"
@@ -55,35 +55,35 @@ resource "google_compute_subnetwork" "tf_vpc_net1_subnet2" {
 }
 
 resource "google_compute_router" "router1" {
-  name     = "ha-vpn-router-1"
-  network  = google_compute_network.tf_vpc_net1.name
+  name    = "ha-vpn-router-1"
+  network = google_compute_network.tf_vpc_net1.name
   bgp {
     asn = var.gcp_asn
   }
 }
 
 resource "google_compute_vpn_tunnel" "tunnel1" {
-  provider              = google-beta
-  name                  = "ha-vpn-tunnel1"
-  region                = "us-central1"
-  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway1.self_link
-  peer_external_gateway = google_compute_external_vpn_gateway.external_gateway.self_link
+  provider                        = google-beta
+  name                            = "ha-vpn-tunnel1"
+  region                          = "us-central1"
+  vpn_gateway                     = google_compute_ha_vpn_gateway.ha_gateway1.self_link
+  peer_external_gateway           = google_compute_external_vpn_gateway.external_gateway.self_link
   peer_external_gateway_interface = 0
-  shared_secret         = var.gcp_shared_secret
-  router                = google_compute_router.router1.self_link
-  vpn_gateway_interface = 0
+  shared_secret                   = var.gcp_shared_secret
+  router                          = google_compute_router.router1.self_link
+  vpn_gateway_interface           = 0
 }
 
 resource "google_compute_vpn_tunnel" "tunnel2" {
-  provider              = google-beta
-  name                  = "ha-vpn-tunnel2"
-  region                = "us-central1"
-  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway1.self_link
-  peer_external_gateway = google_compute_external_vpn_gateway.external_gateway.self_link
+  provider                        = google-beta
+  name                            = "ha-vpn-tunnel2"
+  region                          = "us-central1"
+  vpn_gateway                     = google_compute_ha_vpn_gateway.ha_gateway1.self_link
+  peer_external_gateway           = google_compute_external_vpn_gateway.external_gateway.self_link
   peer_external_gateway_interface = 0
-  shared_secret         = var.gcp_shared_secret
-  router                = google_compute_router.router1.self_link
-  vpn_gateway_interface = 1
+  shared_secret                   = var.gcp_shared_secret
+  router                          = google_compute_router.router1.self_link
+  vpn_gateway_interface           = 1
 }
 
 resource "google_compute_router_interface" "router1_interface1" {
@@ -128,7 +128,7 @@ resource "google_compute_router_peer" "router1_peer2" {
  */
 
 resource "google_compute_instance" "vm_instance1" {
-  name = "terraform-intance-1"
+  name         = "terraform-intance-1"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
 
@@ -148,7 +148,7 @@ resource "google_compute_instance" "vm_instance1" {
 }
 
 resource "google_compute_instance" "vm_instance2" {
-  name = "terraform-intance-2"
+  name         = "terraform-intance-2"
   machine_type = "f1-micro"
   zone         = "us-west1-b"
 
